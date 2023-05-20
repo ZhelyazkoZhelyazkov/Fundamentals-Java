ba1a095e93ece42980fd83d19c5d6f31f052e805
ZhelyazkoZhelyazkov <134138605+ZhelyazkoZhelyazkov@users.noreply.github.com>
2023-05-20 13:58:22 +0300

    Update Ex5.java

--- Ex5.java
+++ Ex5.java
@@ -4,10 +4,39 @@ public class Ex5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-//        You will be given a string representing a username. The password will be that username reversed. Until you receive
-//the correct password, print on the console "Incorrect password. Try again.". When you receive the correct
-//password, print "User {username} logged in." However, on the fourth try, if the password is still not correct,
-//print "User {username} blocked!" and end the program.
+import java.util.Scanner;
+
+        public class Ex5 {
+            public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+
+                String username = scanner.nextLine();
+                String password = "";
+
+                for (int i = username.length() - 1; i >= 0; i--) {
+                    char currentSymbol = username.charAt(i);
+                    password += currentSymbol;
+                }
+
+                int countWrongpass = 0;
+                String lastPassword = scanner.nextLine();
+
+                while (!lastPassword.equals(password)) {
+                    countWrongpass++;
+                    if (countWrongpass == 4) {
+                        System.out.printf("User %s blocked!", username);
+                        break;
+                    }
+                    System.out.println("Incorrect password. Try again.");
+                    lastPassword = scanner.nextLine();
+                }
+                if (lastPassword.equals(password)) {
+                    System.out.printf("User %s logged in.", username);
+//
+                }
+            }
+        }
+
 
     }
 }
